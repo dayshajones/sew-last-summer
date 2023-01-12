2abcc7ec5443d7949680db47e04b7680113368fb
“Daysha <daysha.vu@icloud.com>
2023-01-11 16:57:49 -0800

    working on basic front-end

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return <div>
+        <h1>Sew Last Summer</h1>
+        <p>Home Page</p>
+    </div>
+}
+
+export default Home
---
+++ src/components/ProductContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProductContainer = () => {
+    return (
+    <div>All Products</div>
+    )
+}
+
+export default ProductContainer
